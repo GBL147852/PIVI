7b37b23d19183ba3c7da6af70fcdb4fa38029e4f
lu <unphook@gmail.com>
2017-11-26 22:40:12 -0200

    ações de simulação

--- socorro/main.cpp
+++ socorro/main.cpp
@@ -28,9 +28,13 @@ Texture texture_missile;
 Texture texture_trace;
 
 float zoom = 3;
+
 int simulation_speed = 100;
 bool simulation_running = false;
 bool simulation_ended = false;
+
+double simulation_start_pos[3];
+double simulation_start_vel[3];
 
 std::vector<glm::mat4> simulation_trace;
 std::vector<glm::vec3> simulation_trace_pos;
@@ -41,6 +45,9 @@ std::vector<glm::mat4> simulation_history;
 glm::vec3 missile_pos;
 glm::mat4 missile_mat;
 
+bool press_space = false;
+bool press_enter = false;
+bool press_backspace = false;
 
 // INPUT STATS
 // Initial position : on +Z
@@ -62,7 +69,7 @@ void simulation_start();
 void calc_missile();
 
 void start() {
-	//fundão azul
+	//fundão marelo
 	glClearColor(236.0f/255.0f,230.0f/255.0f,202.0f/255.0f,0.0f);
 	
 	//nossos troço
@@ -75,7 +82,7 @@ void start() {
 	texture_missile = rh_create_texture("missile");
 	texture_trace = rh_create_texture("trace");
 	
-	//inicia simulação
+	//configura simulação
 	sim_radius = 6371000;
 	sim_gravity = 9.807;
 	sim_projectile_drag = 0;
@@ -84,7 +91,12 @@ void start() {
 	sim_read_config("data/config.txt");
 	calc_missile();
 	
-	simulation_start();
+	simulation_start_pos[0] = 0;
+	simulation_start_pos[1] = 0;
+	simulation_start_pos[2] = sim_radius;
+	simulation_start_vel[0] = 6000;
+	simulation_start_vel[1] = 0;
+	simulation_start_vel[2] = 1000;
 }
 
 void end() {
@@ -98,8 +110,8 @@ void end() {
 	rh_delete_texture(texture_trace);
 }
 
-glm::mat4 get_missile_ground() {
-	glm::vec3 direction = glm::normalize(glm::vec3(sim_pos[0],sim_pos[1],sim_pos[2]));
+glm::mat4 get_ground(double pos[3]) {
+	glm::vec3 direction = glm::normalize(glm::vec3(pos[0],pos[1],pos[2]));
 	return glm::translate(glm::mat4(1),direction)*rh_rot(glm::vec3(0,1,0),direction);
 }
 
@@ -115,31 +127,30 @@ void calc_missile() {
 void simulation_start() {
 	sim_generate_wind_seed();
 	
-	//precisa ver como setar esses valores depois
-	sim_pos[0] = 0;
-	sim_pos[1] = sim_radius;
-	sim_pos[2] = 0;
-	sim_vel[0] = 6000;
-	sim_vel[1] = 1000;
-	sim_vel[2] = 0;
+	sim_pos[0] = simulation_start_pos[0];
+	sim_pos[1] = simulation_start_pos[1];
+	sim_pos[2] = simulation_start_pos[2];
+	sim_vel[0] = simulation_start_vel[0];
+	sim_vel[1] = simulation_start_vel[1];
+	sim_vel[2] = simulation_start_vel[2];
 	sim_collide = 0;
 	
 	simulation_running = true;
 	simulation_ended = false;
-	simulation_trace_o = get_missile_ground();
 	simulation_trace_pos.push_back(missile_pos);
 }
 
 void simulation_stop() {
 	simulation_running = false;
 	simulation_ended = false;
+	simulation_trace.clear();
 	simulation_trace_pos.clear();
 }
 
 void simulation_end() {
 	simulation_running = false;
 	simulation_ended = true;
-	simulation_trace_x = get_missile_ground();
+	simulation_trace_x = get_ground(sim_pos);
 	simulation_history.push_back(simulation_trace_x);
 }
 
@@ -204,9 +215,6 @@ bool update() {
 	if(glfwGetKey( window, GLFW_KEY_DOWN ) == GLFW_PRESS){
 		zoom += speed * deltaTime;
 	}
-	if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS){
-		//
-	}
 	
 	// Spooky smooth
 	if(vertical_angle < -vertical_limit) vertical_angle = -vertical_limit;
@@ -215,47 +223,95 @@ bool update() {
 	if(zoom < 2) zoom = 2;
 	if(zoom > 5) zoom = 5;
 	
-	position.x = zoom * cos(vertical_angle) * sin(horizontal_angle);
-	position.y = zoom * sin(vertical_angle);
-	position.z = zoom * cos(vertical_angle) * cos(horizontal_angle);
+	position.x = cos(vertical_angle) * sin(horizontal_angle);
+	position.y = sin(vertical_angle);
+	position.z = cos(vertical_angle) * cos(horizontal_angle);
 	
 	// Camera matrix
 	camera.view = glm::lookAt(
-		position,           // A camera ta aqui
-		glm::vec3(0,0,0), 	// olhando pro centro
-		glm::vec3(0,1,0)    // olhando pra cima
+		position*zoom,		// A camera ta aqui
+		glm::vec3(0,0,0),	// olhando pro centro
+		glm::vec3(0,1,0)	// olhando pra cima
 	);
+	
+	if (glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_1)) {
+		if (!simulation_running) {
+			simulation_clear();
+			simulation_start_pos[0] = position.x*sim_radius;
+			simulation_start_pos[1] = position.y*sim_radius;
+			simulation_start_pos[2] = position.z*sim_radius;
+		}
+	}
+	
+	//hmm
+	if (glfwGetKey(window,GLFW_KEY_SPACE) == GLFW_PRESS) {
+		if (!press_space) {
+			press_space = true;
+			if (simulation_running || simulation_ended) {
+				simulation_stop();
+			} else {
+				simulation_start();
+			}
+		}
+	} else {
+		press_space = false;
+	}
+	bool fastForward = false;
+	if (glfwGetKey(window,GLFW_KEY_ENTER) == GLFW_PRESS) {
+		if (!press_enter) {
+			press_enter = true;
+			fastForward = true;
+			if (simulation_ended) {
+				simulation_stop();
+			}
+			if (!simulation_running) {
+				simulation_start();
+			}
+		}
+	} else {
+		press_enter = false;
+	}
+	if (glfwGetKey(window,GLFW_KEY_BACKSPACE) == GLFW_PRESS) {
+		if (!press_backspace) {
+			press_backspace = true;
+			if (simulation_ended) {
+				simulation_stop();
+			} else {
+				simulation_clear();
+			}
+		}
+	} else {
+		press_backspace = false;
+	}
 	
 	//ok bora simular
 	bool calc_missile_done = false;
 	if (simulation_running) {
-		for (int a = 0; a < simulation_speed; a++) {
-			sim_step(deltaTime);
-			if (sim_collide) {
-				if (!calc_missile_done) {
-					calc_missile_done = true;
-					calc_missile();
+		do {
+			for (int a = 0; a < simulation_speed; a++) {
+				sim_step(deltaTime);
+				if (sim_collide) {
+					if (!calc_missile_done) {
+						calc_missile_done = true;
+						calc_missile();
+					}
+					simulation_end();
+					break;
 				}
-				simulation_end();
-				break;
 			}
-		}
-		if (!calc_missile_done) {
 			calc_missile_done = true;
 			calc_missile();
-		}
-		if (glm::distance(simulation_trace_pos.back(),missile_pos) > 0.05f) {
-			simulation_trace.push_back(missile_mat);
-			simulation_trace_pos.push_back(missile_pos);
-		}
+			if (glm::distance(simulation_trace_pos.back(),missile_pos) > 0.05f) {
+				simulation_trace.push_back(missile_mat);
+				simulation_trace_pos.push_back(missile_pos);
+			}
+		} while (simulation_running && fastForward);
 	}
 	if (!calc_missile_done) {
 		calc_missile_done = true;
 		calc_missile();
 	}
-	if (!simulation_running && !simulation_ended) {
-		simulation_trace_o = get_missile_ground();
-	}
+	simulation_trace_o = get_ground(simulation_start_pos);
 	
 	//desenha terra
 	rh_draw(mesh_sphere,texture_earth,glm::mat4(1));
@@ -267,11 +323,11 @@ bool update() {
 	}
 	
 	//desenha ponto de início
-	rh_draw(mesh_trace_o,texture_trace,simulation_trace_o*glm::scale(glm::mat4(1),glm::vec3(.05f)));
+	rh_draw(mesh_trace_o,texture_trace,simulation_trace_o*glm::scale(glm::mat4(1),glm::vec3(.025f)));
 	
 	//desenha ponto de fim
 	if (simulation_ended) {
-		rh_draw(mesh_trace_x,texture_trace,simulation_trace_x*glm::scale(glm::mat4(1),glm::vec3(.05f)));
+		rh_draw(mesh_trace_x,texture_trace,simulation_trace_x*glm::scale(glm::mat4(1),glm::vec3(.025f)));
 	}
 	
 	//desenha rastros
@@ -281,7 +337,7 @@ bool update() {
 	
 	//desenha histórico de simulações (pontos finais)
 	for (int a = 0; a < simulation_history.size(); a++) {
-		rh_draw(mesh_trace_o,texture_trace,simulation_history[a]*glm::scale(glm::mat4(1),glm::vec3(.01f,.01f,.01f)));
+		rh_draw(mesh_trace_o,texture_trace,simulation_history[a]*glm::scale(glm::mat4(1),glm::vec3(.005f)));
 	}
 	
 	return true;
